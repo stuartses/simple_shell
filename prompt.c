503fb56b0b6c42768c24f5f1eacd9ba00b07fd83
stuartses <stuart.ses@hotmail.com>
2019-11-18 21:38:40 +0000

    getline

--- prompt.c
+++ prompt.c
@@ -1,16 +1,23 @@
-#include <stdlib.h>
-#include <stdio.h>
-int main (void)
+#include <holberton.h>
+/**
+ * prompt - get from standard input
+ *
+ * Description: get string from standard input
+ * Return: string
+ */
+char *prompt(void)
 {
 	char *buffer;
-	size_t buffer_size = 50;
-	size_t getline_out;
+	size_t buffer_size = BUFFERSIZE, getline_out;
+	int buff_len = 0;
 
 	buffer = malloc(sizeof(char) * buffer_size);
 	printf("$ ");
 	getline_out = getline(&buffer, &buffer_size, stdin);
-	printf("%s", buffer);
-	free(buffer);
 
-	return (0);
+	while (buffer[buff_len] != '\n')
+		buff_len++;
+	buffer[buff_len] = '\0';
+
+	return (buffer);
 }
